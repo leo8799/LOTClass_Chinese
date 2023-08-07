774ef6ac421c19fbcd60b162127456aa38b72be1
Bell2016411026 <2873858230@qq.com>
2023-08-07 11:04:16 +0800

    修改了mcp

--- test.py
+++ test.py
@@ -4,3 +4,4 @@ trainer = LOTClassTrainer(args)
 trainer.category_vocabulary(top_pred_num=args.train_args.top_pred_num,
                             category_vocab_size=args.train_args.category_vocab_size)
 trainer.self_train(epochs=args.train_args.SELF_TRAIN_EPOCH, loader_name=args.data.final_model)
+trainer.mcp(50,20,5,"mcp_model.pt")
